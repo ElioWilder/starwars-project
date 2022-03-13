095a55501a015a8dbee624685f382e5c369cb095
Ylisdra <laura.morvan.fr@gmail.com>
2022-03-13 18:58:46 +0100

    update app.js + create file component js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Component/Header/header.js';
+import Footer from './Component/Footer/footer.js';
+import Search from './Component/Search/search.js'
+import CardListPage from './Component/CardListPage/cardlistpage.js';
+/*import CardList from './Component/CardList/cardlist.js';*/
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Search />
-      <CardListPage />
+      <body>
+        <Search />
+        <CardListPage />
+      </body>
       <Footer />
     </div>
   );
