cbac17fcc244c619b3b19bc9860f24698ab8035e
Ylisdra <laura.morvan.fr@gmail.com>
2022-03-20 14:28:42 +0100

    use react-query for api

--- src/App.js
+++ src/App.js
@@ -2,21 +2,44 @@ import './App.css';
 import React from 'react';
 import Header from './Component/Header/header.js';
 import Footer from './Component/Footer/footer.js';
-import Search from './Component/Search/search.js'
-import CardListPage from './Component/CardListPage/cardlistpage.js';
-/*import CardList from './Component/CardList/cardlist.js';*/
+import StarwarsListPage from './Component/starwarsListPage/StarwarsListPage.js';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
-function App() {
-  return (
+ const queryClient = new QueryClient();
+
+ export default function App() {
+
+   return (
+
+     <QueryClientProvider client={queryClient}>
+       <Example />
+     </QueryClientProvider>
+
+   )
+
+ }
+
+ function Example() {
+
+   const { isLoading, error, data } = useQuery('repoData', () =>
+
+     fetch('https://swapi.dev/api/').then(res => res.json())
+   );
+
+   if (isLoading) return 'Loading...';
+
+   if (error) return 'An error has occurred: ' + error.message;
+
+   return (
+
     <div className="App">
       <Header />
       <div>
-        {/* <Search /> */}
-        {/* <CardListPage /> */}
+        {/* <StarwarsListPage /> */}
       </div>
       <Footer />
     </div>
-  );
-}
 
-export default App;
+   )
+
+ }
--- src/Component/Header/header.js
+++ src/Component/Header/header.js
@@ -3,6 +3,7 @@ import logo from './logo.png';
 const Header = () => {
     return <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+                <p>Le wiki des étoiles</p>
             </header>
 };
 
