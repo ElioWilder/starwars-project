cafe1020bbf77063911a71fe77b902ab616cf82d
Ylisdra <laura.morvan.fr@gmail.com>
2022-03-27 19:13:22 +0200

    update css

--- src/Component/starwarsListPage/starwarsList/starwarsCard/detailsPage/DetailsPage.js
+++ src/Component/starwarsListPage/starwarsList/starwarsCard/detailsPage/DetailsPage.js
@@ -6,6 +6,8 @@ import CardActions from '@mui/material/CardActions';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import Header from "../../../../Header/header.js";
+import Footer from "../../../../Footer/footer.js";
 
 const DetailsPage = () => {
 
@@ -21,28 +23,57 @@ const DetailsPage = () => {
    const person = data; 
     
     
-   return (
-       <Box sx={{display: 'flex', justifyContent: 'center', backgroundColor: '#b39ddb'}}>
-            <Card sx={{ width: 500, height: 500, p: 2, m: 2, display: 'flex', flexDirection: 'column'}}>
-                <CardHeader title={person.name} sx={{textAlign: 'center'}} />
-                <CardContent sx={{textAlign: 'center'}}>
-                    <h3>Etat civil</h3>
+   
+    if(person.gender !== 'n/a' && person.hair_color !== 'n/a' && person.hair_color !=='none'){
+        return (
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: '#b39ddb'}}>
+                <Header/>
+                <Card sx={{ width: 500, height: 500, p: 2, m: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <CardHeader title={person.name} sx={{textAlign: 'center', color: '#b39ddb'}} />
+                    <CardContent sx={{textAlign: 'center'}}>
+                        <h3>Etat civil</h3>
                         <div>Genre :{person.gender}</div>
                         <div>Année de naissance :{person.birth_year}</div>
-                    <h3>Taille et poids</h3>    
+                        <h3>Taille et poids</h3>
                         <div>Taille :{person.height}</div>
                         <div>Poids :{person.mass}</div>
-                    <h3>Aspect Physique</h3>
+                        <h3>Aspect Physique</h3>
                         <div>Couleur de cheveux :{person.hair_color}</div>
                         <div>Couleur de peau :{person.skin_color}</div>
                         <div>Couleur des yeux :{person.eye_color}</div>
-                </CardContent>
-                <CardActions>
-                    <Button size="medium" color="secondary" variant="contained"><Link to='/'>Retour</Link></Button>
-                </CardActions>
-            </Card> 
-        </Box>       
-    );
+                    </CardContent>
+                    <CardActions>
+                        <Button size="medium" variant="contained"><Link to='/'>Retour</Link></Button>
+                    </CardActions>
+                </Card>
+                <Footer />
+            </Box>
+        );
+    } else {
+        return (
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: '#b39ddb'}}>
+                <Header/>
+                <Card sx={{ width: 500, height: 500, p: 2, m: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <CardHeader title={person.name} sx={{textAlign: 'center', color: '#b39ddb'}} />
+                    <CardContent sx={{textAlign: 'center'}}>
+                        <h3>Etat civil</h3>
+                        <div>Genre : Robot</div>
+                        <div>Année de naissance :{person.birth_year}</div>
+                        <h3>Taille et poids</h3>
+                        <div>Taille :{person.height}</div>
+                        <div>Poids :{person.mass}</div>
+                        <h3>Aspect Physique</h3>
+                        <div>Couleur de peau :{person.skin_color}</div>
+                        <div>Couleur des yeux :{person.eye_color}</div>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="medium" variant="contained"><Link to='/'>Retour</Link></Button>
+                    </CardActions>
+                </Card>
+                <Footer />
+            </Box>
+        );
+    }      
 }
 
 export default DetailsPage;
